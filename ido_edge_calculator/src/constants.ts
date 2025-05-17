8b3bf1930243d231c3e7279ef10b96a127170973
Ravnsgaard <j@cphmist.com>
2025-05-17 20:25:17 +0200

    Delete ido_edge_calculator directory

--- ido_edge_calculator/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const COMMIT_MULTIPLIER = { "3yr": 1.0, "1yr": 1.2, monthly: 1.44 } as const;
-export const OVERAGE = { trafficPerTB: 400, computeBlocksPack: 400, computeBlocksPackSize: 10 } as const;
-export type CommitTerm = keyof typeof COMMIT_MULTIPLIER;
-export type Currency = "EUR" | "USD" | "DKK";
